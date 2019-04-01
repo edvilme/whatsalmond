b721d0fa796fb6bb14f2fe77e74e065b93fd6a28
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2019-04-01 06:48:42 -0600

    fixed bugs

--- main.js
+++ main.js
@@ -451,7 +451,7 @@ app.post("/wa", function(req, res){
 		}
 	}*/
 	res.writeHead(200, {'Content-Type': 'text/xml'});
-	twiml.message(req)
+	twiml.message("req.body")
 	res.end(twiml.toString())
 })
 
