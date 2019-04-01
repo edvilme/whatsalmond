79a0eb358b25c8a153a76fc657cc564f8962fac3
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2019-04-01 07:33:19 -0600

    beta release debug

--- main.js
+++ main.js
@@ -432,6 +432,9 @@ app.post("/wa", function(req, res){
 				console.log("sent")
 			}
 		}
+		users[phoneID]['ws'].on("message", function(e){
+			console.log(e)
+		})
 		function connectWS(){
 			var wasOpen=false;
 			var reconnectTimeout=100;
@@ -455,7 +458,7 @@ app.post("/wa", function(req, res){
 					})
 					//res.end(JSON.stringify(response))
 				} */
-				console.log("received")
+				//console.log("received")
 				//twiml.message("message")
 			}
 		}
